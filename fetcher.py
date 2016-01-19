82d412e0f3267184b026a1e351b414eaecb3bd67
Michael Prattipati <michael.prattipati@gmail.com>
2016-01-18 22:21:23 -0800

    GET request added to fetch box scores

--- fetcher.py
+++ fetcher.py
@@ -9,3 +9,8 @@ key = key.encode('ascii')
 req = urllib.request.Request(url, key)
 with urllib.request.urlopen(req) as response:
     message = response.read
+
+# GET request for data
+results = json.loads(urllib.request.urlopen("https://www.kimonolabs.com/api/7kwrt2m6?apikey=m7HpuKp7CCQdG3yGRArJQzu7nqDWczAX").read().decode("utf-8"))
+#print(message)
+print(results)
